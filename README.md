b8f761c9102cea27a2744059ea8affad652e5f98
Andrew Peterson <andypeterson526@gmail.com>
2021-09-19 15:10:54 -0700

    API voice recognition proof of concept works

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # ktane_bot_ts
 AI assistant for Keep Talking and Nobody Explodes. A (hopefully) simple voice recognition bot to respond to basic user input and assist in bomb defusal for Keep Talking and Nobody Explodes. If I can't finish this project by the time FA21 starts then this project will likely be abandoned until I have free time. This is mainly a passion project for myself since nobody ever wants to play KTANE for more than 5 min at a time.
+---
+Basic installation process:
+ - Make sure you have the google cloud API installed via [this](https://cloud.google.com/sdk/docs/install)
+ - Make sure you have a project created and configured to your machine that uses the speech-to-text API
+ - Install SoX and have it available in your $PATH
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,68 @@
-console.log("Hello, world!");
+// Copyright 2017 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Clone of https://github.com/googleapis/nodejs-speech/blob/HEAD/samples/recognize.js for me to test to make sure my config + API setup worked fine
+
+import * as recorder from "node-record-lpcm16";
+
+// Imports the Google Cloud client library
+import * as speech from "@google-cloud/speech";
+
+// Creates a client
+const client = new speech.SpeechClient();
+
+/**
+ * TODO(developer): Uncomment the following lines before running the sample.
+ */
+const encoding = "LINEAR16";
+const sampleRateHertz = 16000;
+const languageCode = "en-US";
+
+const request = {
+  config: {
+    encoding: encoding as any,
+    sampleRateHertz: sampleRateHertz,
+    languageCode: languageCode,
+  },
+  interimResults: true, // If you want interim results, set this to true
+};
+
+// Create a recognize stream
+const recognizeStream = client
+  .streamingRecognize(request)
+  .on("error", console.error)
+  .on("data", (data) =>
+    process.stdout.write(
+      data.results[0] && data.results[0].alternatives[0]
+        ? `Transcription: ${data.results[0].alternatives[0].transcript}\n`
+        : "\n\nReached transcription time limit, press Ctrl+C\n"
+    )
+  );
+
+// Start recording and send the microphone input to the Speech API.
+// Ensure SoX is installed, see https://www.npmjs.com/package/node-record-lpcm16#dependencies
+recorder
+  .record({
+    sampleRateHertz: sampleRateHertz,
+    threshold: 0,
+    // Other options, see https://www.npmjs.com/package/node-record-lpcm16#options
+    verbose: false,
+    recordProgram: "rec", // Try also "arecord" or "sox"
+    silence: "10.0",
+  })
+  .stream()
+  .on("error", console.error)
+  .pipe(recognizeStream);
+
+console.log("Listening, press Ctrl+C to stop.");
